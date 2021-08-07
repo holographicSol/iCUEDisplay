03c85faa0ea3928696bddfb0de1aae0bc6610c3c
holographic_sol <88397088+holographicSol@users.noreply.github.com>
2021-08-07 07:50:03 +0100

    Add files via upload

--- iCUEDisplay.py
+++ iCUEDisplay.py
@@ -296,6 +296,7 @@ config_data = ['sdk_color_cpu_on: 255,255,0',
 def create_new():
     global bool_backend_install
     print('-- [create_new]: started')
+    distutils.dir_util.mkpath(os.path.join(os.path.expanduser('~'), 'AppData\\Local\\iCUEDisplay'))
     config_line_missing = []
     config_line_file = []
     count_missing = 0
@@ -379,9 +380,10 @@ def create_new():
                 time.sleep(2)
                 bool_backend_install = True
 
+    app_data_path = os.path.join(os.path.expanduser('~'), 'AppData\\Local\\iCUEDisplay\\icue_display_py_config.dat')
     py_config_line = os.path.join(os.getcwd()+'\\py\\temp_sys.dat')
-    open('C:\\py_config.dat', 'w').close()
-    with open('C:\\py_config.dat', 'a') as fo:
+    open(app_data_path, 'w').close()
+    with open(app_data_path, 'a') as fo:
         fo.writelines('PATH: '+py_config_line)
     fo.close()
 
@@ -5250,7 +5252,7 @@ class TemperatureClass(QThread):
         self.gpu_core = ''
 
     def send_instruction(self):
-        print('-- [TemperatureClass.send_instruction]: plugged in')
+        # print('-- [TemperatureClass.send_instruction]: plugged in')
         if self.cpu_pack != '':
             self.cpu_pack_0 = self.cpu_pack.split()
             self.cpu_pack_1 = self.cpu_pack_0[-1]
